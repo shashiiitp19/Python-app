badaea3287e53e54717615351b59b81e906c9fdd
Shashi Kumar <shashi.kumar@skypointcloud.com>
2024-11-14 14:35:11 +0530

    added strings in return for lint

--- fastapi-app/main.py
+++ fastapi-app/main.py
@@ -1,13 +1,18 @@
+"""
+Main application module for FastAPI.
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/add")
 def add(a: float, b: float):
+    """return the result after addition"""
     return {"result": a + b}
 
 @app.get("/multiply")
 def multiply(a: float, b: float):
+    """return the result after multiply"""
     return {"result": a * b}
 
 
